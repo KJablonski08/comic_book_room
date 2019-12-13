11075a4084da7e72da8ab6070701376438953790
KJablonski08 <kjablonski08@gmail.com>
2019-12-12 21:25:37 -0500

    add user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,54 @@
-class User < ApplicationController 
+class UsersController < ApplicationController
 
-  def index 
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @users = User.all 
   end 
 
   def show 
   end 
 
   def new 
+    @user = User.new
   end 
 
   def create 
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "Welcome to the Comic Book Room #{@user.username}"
+      redirect_to user_path(@user)
+    else 
+      render 'new'
+    end 
   end 
 
   def edit 
   end 
 
   def update 
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated"
+      redirect_to user_path(@user)
+    else 
+      render 'edit'
+    end 
   end 
 
   def destroy 
+    @user.destroy
+    flash[:danger] = "User account was successfully destroyed"
+    redirect_to users_path
   end 
 
   private 
 
   def set_user 
+    @user =User.find(params[:id])
   end 
 
   def user_params 
+    params.require(:user).permit(:username, :name, :email, :password)
   end 
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static#home'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :comics
+
+  resources :users, except: [:new]
 
 end
