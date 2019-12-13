fcf6bdddf9e6aa78c2475049d3e9ac9128edf3a9
KJablonski08 <kjablonski08@gmail.com>
2019-12-12 19:25:44 -0500

    create users model and table

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base 
+
+  validates :username, presence: true, uniqueness: { case_sensative: false }, length: {minimum: 5, maximum: 25}
+  VALID_EMAIL__REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, uniqueness: {case_sensative: false}, length: {maximum: 105}, format: { with: VALID_EMAIL__REGEX }
+
+  has_secure_password
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_034647) do
+ActiveRecord::Schema.define(version: 2019_12_13_000855) do
 
   create_table "comics", force: :cascade do |t|
     t.string "series_title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_12_09_034647) do
     t.boolean "anonymous", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "password_digest"
   end
 
 end
